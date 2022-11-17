c84e0b980946b1c5a5eca542be8c3f1772241289
Stalin Chiguano <schiguano@metropolitan-touring.com>
2022-11-17 12:19:04 -0500

    validacion formulario login

--- src/containes/Login.tsx
+++ src/containes/Login.tsx
@@ -2,19 +2,19 @@ import backgroundImage from '../assets/img/login_img.svg';
 import '../assets/styles/Login.css';
 import {useDispatch, useSelector} from "react-redux";
 import {LOGIN_INGRESAR, LOGIN_LOAD_INITIAL_DATA} from "../store/login/types";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {RootState} from "../store";
 import {objectIsVoid} from "../helpers/utilsObject";
 import {useNavigate} from "react-router-dom";
-
-interface ILogin {
-    email: string;
-    password: string;
-}
+import {useForm} from 'react-hook-form';
 
 const Login = () => {
 
-    const [formLogin, setFormLogin] = useState({} as ILogin);
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm();
 
     const {
         usuarioLogueado,
@@ -37,37 +37,59 @@ const Login = () => {
         }
     }, [usuarioLogueado]);
 
+    const handleLogin = (e: any) => {
+        dispatch({
+            type: LOGIN_INGRESAR,
+            payload: e
+        })
+    }
+
     return (
         <section className="login">
             <figure className="login__picture">
                 <img className="login__img" src={backgroundImage} alt="contact img"/>
             </figure>
-            <form className="login__form">
+            <form className="login__form" onSubmit={handleSubmit(handleLogin)}>
                 <h2 className="login__title">Iniciar Sesión</h2>
                 <label className="login__text">
                     Correo Electrónico
                     <input
                         placeholder="juan@gmail.com"
                         className="login__input"
-                        onChange={(e) => setFormLogin({...formLogin, email: e.target.value})}
+                        {...register('email', {
+                            required: true,
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: 'Email invalido'
+                            }
+                        })}
                     />
                 </label>
+                {errors?.email?.type === 'required' && (
+                    <p className="login__error">Este campo es requerido</p>
+                )}
+                {errors.email?.message && <p className="login__error">{errors.email?.message + ""}</p>}
+
                 <label className="login__text">
                     Contraseña
                     <input
                         placeholder="********"
                         className="login__input"
-                        onChange={(e) => setFormLogin({...formLogin, password: e.target.value})}
+                        {...register('password', {
+                            required: true,
+                            minLength: {
+                                value: 6,
+                                message: 'La contraseña debe tener almenos 6 caracteres'
+                            }
+                        })}
                     />
                     <i className="bx bx-show login__password--show"/>
                 </label>
-                <button type="button" className="login__cta"
-                        onClick={() => {
-                            dispatch({
-                                type: LOGIN_INGRESAR,
-                                payload: formLogin
-                            })
-                        }}>
+                {errors?.password?.type === 'required' && (
+                    <p className="login__error">Este campo es requerido</p>
+                )}
+                {errors.password?.message && <p className="login__error">{errors.password?.message + ""}</p>}
+                <button type="submit" className="login__cta">
                     Login
                 </button>
             </form>
