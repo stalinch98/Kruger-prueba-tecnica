bca7e8ad5c208f9728161a0417ea42fd685740f1
Stalin Chiguano <schiguano@metropolitan-touring.com>
2022-11-17 20:49:09 -0500

    validacion formulario administrador

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -25,14 +25,10 @@ P
  src/store/administrador/types.ts,3\6\3673c0cf2cb75e1c2f9d37cb3bf21a8d7d2f0b34
 N
 src/store/login/LoginReduce.ts,8\9\89cf09cfc1d6e2deb4b62b87d639a6ab8f50d460
-K
-src/assets/styles/Login.css,f\3\f3b02778332420a5d55d14a9e9665b8d504e1e59
 ^
 .src/store/administrador/AdministradorReduce.ts,a\8\a8b397ae3ea1c8b8d7f0be67988e725730f2975f
 R
 "src/store/administrador/actions.ts,f\a\faa920a70cdc951e05c52f5e90b363deb5855185
-S
-#src/assets/styles/Administrador.css,a\6\a6c30241e301eb1911fab3822517e6c016ad737b
 M
 src/store/empleado/actions.ts,d\7\d7370367b5f20645ab60243cf31d6e66c9fac7da
 O
@@ -47,14 +43,8 @@ I
 src/store/rootReducers.ts,b\7\b77576fb2d8aa0c740390b760becca93498bbb48
 T
 $src/store/empleado/EmpleadoReduce.ts,3\e\3ebb620e8a640a150ba85a59e66ac87aa1ba4a99
-O
-src/interfaces/administrador.ts,5\b\5b3078a214bccfdd3ab0ecf9f630cd0ce192a56b
 I
 src/interfaces/general.ts,d\c\dca51f5fdf805a3bb4a7afe9aa54383bcca07fcb
-Y
-)src/components/ModalActualizarUsuario.tsx,4\a\4a2a3fe09bcbcf5f0f3a23637d353d826cad37fa
-G
-src/interfaces/login.ts,0\7\07a7a62fd3fbeeed93ecc11a8623c10d07dc7c32
 I
 src/components/Layout.tsx,b\4\b4694ebb3940bc08ad73f040eaf0c8d049cc8dab
 F
@@ -63,11 +53,17 @@ K
 src/store/empleado/types.ts,7\f\7f2f9e5d1cf7aec123b96bcae8f9f316b6a4d93f
 J
 src/helpers/utilsObject.ts,4\2\420c5ae9c2a6b0329911b6c5663c68f4383a509f
-\
-,src/store/administrador/AdministradorSaga.ts,b\1\b16ce531f864d0d7f34ad967fbb0c296294a3c86
 R
 "src/store/empleado/EmpleadoSaga.ts,b\e\be6f7feff6186ccc7e0cf34ed0a13605c647eb47
 L
 src/store/login/loginSaga.ts,9\0\90057e7410a43175c4b32757095ffd534e205022
 P
  src/components/ModalEmpleado.tsx,a\0\a0579340356ffc60e2370c7f9ad1ad027fa94f90
+N
+src/assets/files/usuarios.json,6\b\6b2dbf33b658bb3a3b2c43cb020c9d0475481c6d
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+9
+	yarn.lock,5\3\53585a096fe3a3b96321113856ae62602995a33b
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
--- src/components/ModalActualizarUsuario.tsx
+++ src/components/ModalActualizarUsuario.tsx
@@ -1,32 +1,48 @@
 import Modal from "react-bootstrap/Modal";
-import {setCurrentUser, setOpenModal} from "../store/administrador/actions";
+import {setCurrentUser, setOpenModal, setIsEdit} from "../store/administrador/actions";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../store";
-import {useEffect, useState} from "react";
 import {Usuario} from "../interfaces/login";
 import {ADMINISTRADOR_CREAR_USUARIO, ADMINISTRADOR_SET_USERS} from "../store/administrador/types";
+import {useForm} from "react-hook-form";
+import {useEffect} from "react";
 
 const ModalActualizarUsuario = () => {
 
     const dispatch = useDispatch();
 
     const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors}
+    } = useForm();
+
+    const {
         openModal, currentUser, isEdit
     } = useSelector((state: RootState) => state.AdministradorReduce);
 
-    const [updatedUser, setUpdatedUser] = useState<Usuario>({} as Usuario);
 
-    useEffect(() => {
-        setUpdatedUser(currentUser);
-    }, [isEdit, openModal]);
-
-    const handleSubmit = () => {
+    const handleSaveUpdateUser = (e: any) => {
         if (isEdit) {
-            dispatch({type: ADMINISTRADOR_SET_USERS, payload: updatedUser});
+            dispatch({type: ADMINISTRADOR_SET_USERS, payload: e});
         } else {
-            dispatch({type: ADMINISTRADOR_CREAR_USUARIO, payload: updatedUser});
+            dispatch({type: ADMINISTRADOR_CREAR_USUARIO, payload: e});
         }
     }
+
+    useEffect(() => {
+        if (isEdit) {
+            reset(currentUser);
+        } else {
+            reset({
+                cedula: '',
+                nombres: '',
+                apellidos: '',
+                email: '',
+            });
+        }
+    }, [currentUser]);
 
     return (
         <div>
@@ -38,49 +54,94 @@ const ModalActualizarUsuario = () => {
             >
                 <Modal.Header closeButton onClick={() => {
                     dispatch(setCurrentUser({} as Usuario));
+                    dispatch(setIsEdit(false));
                     dispatch(setOpenModal(false));
                 }}>
                     <Modal.Title id="contained-modal-title-vcenter">
                         {isEdit ? 'Editar usuario' : 'Crear usuario'}
                     </Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    <form>
+                <form onSubmit={handleSubmit(handleSaveUpdateUser)}>
+                    <Modal.Body>
                         <div className="row">
                             <div className="col">
                                 <label htmlFor="formGroupExampleInput">Nombres</label>
-                                <input type="text" className="form-control" defaultValue={updatedUser.nombres}
-                                       onChange={(e) => setUpdatedUser({...updatedUser, nombres: e.target.value})}/>
+                                <input type="text" className="form-control" defaultValue={currentUser.nombres}
+                                       {...register('nombres', {
+                                           required: true,
+                                           pattern: {
+                                               value: /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/,
+                                               message: 'Nombres invalidos'
+                                           }
+                                       })}
+                                />
+                                {errors?.nombres?.type === 'required' && (
+                                    <p className="administrador__error">Este campo es requerido</p>
+                                )}
+                                {errors.nombres?.message &&
+                                    <p className="administrador__error">{errors.nombres?.message + ""}</p>}
                             </div>
                             <div className="col">
                                 <label htmlFor="formGroupExampleInput">Apellidos</label>
-                                <input type="text" className="form-control" defaultValue={updatedUser.apellidos}
-                                       onChange={(e) => setUpdatedUser({...updatedUser, apellidos: e.target.value})}/>
+                                <input type="text" className="form-control" defaultValue={currentUser.apellidos}
+                                       {...register('apellidos', {
+                                           required: true,
+                                           pattern: {
+                                               value: /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/,
+                                               message: 'Apellidos invalidos'
+                                           }
+                                       })}
+                                />
+                                {errors?.apellidos?.type === 'required' && (
+                                    <p className="administrador__error">Este campo es requerido</p>
+                                )}
+                                {errors.apellidos?.message &&
+                                    <p className="administrador__error">{errors.apellidos?.message + ""}</p>}
                             </div>
                         </div>
                         <div className="row">
                             <div className="col">
                                 <label htmlFor="formGroupExampleInput">Cédula</label>
-                                <input type="text" className="form-control" defaultValue={updatedUser.cedula}
-                                       onChange={(e) => setUpdatedUser({...updatedUser, cedula: e.target.value})}/>
+                                <input type="text" className="form-control" defaultValue={currentUser.cedula}
+                                       {...register('cedula', {
+                                           required: true,
+                                           pattern: {
+                                               value: /^[0-9]{10,10}$/g,
+                                               message: 'Cédula invalida'
+                                           }
+                                       })}
+                                />
+                                {errors?.cedula?.type === 'required' && (
+                                    <p className="administrador__error">Este campo es requerido</p>
+                                )}
+                                {errors.cedula?.message &&
+                                    <p className="administrador__error">{errors.cedula?.message + ""}</p>}
                             </div>
                             <div className="col">
                                 <label htmlFor="formGroupExampleInput">Email</label>
-                                <input type="text" className="form-control" defaultValue={updatedUser.email}
-                                       onChange={(e) => setUpdatedUser({...updatedUser, email: e.target.value})}/>
+                                <input type="text" className="form-control" defaultValue={currentUser.email}
+                                       {...register('email', {
+                                           required: true,
+                                           pattern: {
+                                               value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                               message: 'Email invalido'
+                                           }
+                                       })}
+                                />
+                                {errors?.email?.type === 'required' && (
+                                    <p className="administrador__error">Este campo es requerido</p>
+                                )}
+                                {errors.email?.message &&
+                                    <p className="administrador__error">{errors.email?.message + ""}</p>}
                             </div>
                         </div>
-                    </form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <button className="btn btn-secondarybtn btn-secondary" onClick={() => {
-                        dispatch(setCurrentUser({} as Usuario));
-                        dispatch(setOpenModal(false));
-                    }}>Cerrar
-                    </button>
-                    <button className="btn btn-secondarybtn btn-primary" onClick={handleSubmit}>Guardar
-                    </button>
-                </Modal.Footer>
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button className="btn btn-secondarybtn btn-primary" type="submit">Guardar
+                        </button>
+                    </Modal.Footer>
+                </form>
             </Modal>
         </div>
     )
